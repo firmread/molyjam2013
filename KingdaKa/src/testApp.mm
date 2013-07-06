85d3f3aee60e6e1a2b30c1679a5bcaa054815a72
Peng Cheng <bestpaul1985@gmail.com>
2013-07-06 16:33:12 -0400

    control 02

--- KingdaKa/src/testApp.mm
+++ KingdaKa/src/testApp.mm
@@ -19,6 +19,14 @@ void testApp::setup(){
     rect_down.set(0, 512, 768, 512);
     
     touch_num_up = 0;
+    
+    string myString[3];
+    myString[0] = "rock";
+    myString[1] = "paper";
+    myString[2] = "scissors";
+
+    command_up = myString[int(ofRandom(0, 2))];
+    
 }
 
 //--------------------------------------------------------------
@@ -38,10 +46,6 @@ void testApp::update(){
         particles[i].update();
     }
     
-    
-   
-    
-//cout<<"touch_num_up:  "<<touch_num_up<<"  fingers Number:  "<<mfinger_up.size()<<endl;
 }
 
 //--------------------------------------------------------------
@@ -49,7 +53,18 @@ void testApp::draw(){
     
     
     ofSetColor(247, 255, 236);
-    ofCircle(particles[0].pos.x,particles[0].pos.y, 10);
+    if (command_up == "rock") {
+        ofCircle(particles[0].pos.x,particles[0].pos.y, 30);
+    }
+    else if(command_up == "paper"){
+        ofSetRectMode(OF_RECTMODE_CENTER);
+        ofRect(particles[0].pos.x, particles[0].pos.y, 60, 60);
+    }
+    else if(command_up == "scissors"){
+        ofTriangle(particles[0].pos.x-60, particles[0].pos.y, particles[0].pos.x+60, particles[0].pos.y, particles[0].pos.x, particles[0].pos.y+60);
+    }
+    
+    
     ofCircle(particles[1].pos.x,particles[1].pos.y, 10);
     
 }
@@ -61,54 +76,69 @@ void testApp::touchDown(ofTouchEventArgs & touch){
     
     
     if (rect_up.inside(touch.x, touch.y)) {
-        finger temp;
-        temp.ID = touch.id;
-        temp.district = 0;
-        temp.pos.set(touch.x, touch.y);
-        temp.prePos.set(touch.x, touch.y);
-        mfinger_up.push_back(temp);
-    
+            finger temp;
+            temp.ID = touch.id;
+            temp.district = 0;
+            temp.pos.set(touch.x, touch.y);
+            temp.prePos.set(touch.x, touch.y);
+            mfinger_up.push_back(temp);
     }
-    
     
 }
 
 //--------------------------------------------------------------
 void testApp::touchMoved(ofTouchEventArgs & touch){
     
-    for (int i =0; i<mfinger_up.size(); i++){
-        if (touch.id == mfinger_up[i].ID) {
-            mfinger_up[i].pos.set(touch.x, touch.y);
+    if (mfinger_up.size()>1){
+        for (int i =0; i<mfinger_up.size(); i++){
+            if (touch.id == mfinger_up[i].ID) {
+                mfinger_up[i].pos.set(touch.x, touch.y);
+                
+                if (mfinger_up.size()>1) {
+                    float dis =  mfinger_up.back().pos.distance(mfinger_up[mfinger_up.size()-2].pos);
+                    float preDis =  mfinger_up.back().prePos.distance(mfinger_up[mfinger_up.size()-2].prePos);
+                    float diff = preDis - dis;
+                    if (diff > 10) {
+                        command_up = "rock";
+                    }else if(diff < -10){
+                        command_up = "paper";
+                    }
+                }
+            }
         }
     }
    
-    if (mfinger_up.size()>1) {
-            float dis =  mfinger_up.back().pos.distance(mfinger_up[mfinger_up.size()-2].pos);
-            float preDis =  mfinger_up.back().prePos.distance(mfinger_up[mfinger_up.size()-2].prePos);
-            float diff = preDis - dis;
-            if (diff > 10) {
-                 cout<<"rock_up"<<endl;
-            }
-    }
-  
-    for (int i =0; i<mfinger_up.size(); i++){
-        if (touch.id == mfinger_up[i].ID) {
-            mfinger_up[i].prePos.set(touch.x, touch.y);
-        }
-    }
+    
+    
     
 }
 
 //--------------------------------------------------------------
 void testApp::touchUp(ofTouchEventArgs & touch){
+    
+    if (mfinger_up.size()>1) {
+        for (int i =0; i<mfinger_up.size(); i++){
+            if (touch.id == mfinger_up[i].ID) {
+                mfinger_up[i].pos.set(touch.x, touch.y);
+                if (mfinger_up.size()>1) {
+                    float dis =  mfinger_up.back().pos.distance(mfinger_up[mfinger_up.size()-2].pos);
+                    float preDis =  mfinger_up.back().prePos.distance(mfinger_up[mfinger_up.size()-2].prePos);
+                    float diff = preDis - dis;
+                    if (diff < 30 && diff > -30) {
+                        command_up = "scissors";
+                    }
+                }
+            }
+        }
 
+    }
     
     for (int i =0; i<mfinger_up.size(); i++) {
         if (mfinger_up[i].district == 0 && touch.id == mfinger_up[i].ID) {
                 mfinger_up.erase(mfinger_up.begin()+i);
         }
     }
-
+ 
     
     
 }
