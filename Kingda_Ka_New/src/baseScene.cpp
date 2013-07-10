5bda108ae9f50c0827932c77295c73bc54bc5db2
Peng Cheng <bestpaul1985@gmail.com>
2013-07-10 15:30:04 -0400

    new file

---
+++ Kingda_Ka_New/src/baseScene.cpp
@@ -0,0 +1,9 @@
+//
+//  baseScene.cpp
+//  Kingda_Ka_New
+//
+//  Created by PengCheng on 7/10/13.
+//
+//
+
+#include "baseScene.h"
---
+++ Kingda_Ka_New/src/menu.cpp
@@ -0,0 +1,9 @@
+//
+//  menu.cpp
+//  Kingda_Ka_New
+//
+//  Created by PengCheng on 7/10/13.
+//
+//
+
+#include "menu.h"
